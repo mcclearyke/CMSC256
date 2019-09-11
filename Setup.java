baeda92e19b127cdf3f202d2cd39c9b5bd01a9bf
mcclearyke <46753320+mcclearyke@users.noreply.github.com>
2019-09-11 12:49:04 -0400

    Update Setup.java

--- Setup.java
+++ Setup.java
@@ -1,9 +1,16 @@
+/********************************
+This was LAB 4
+- line/block comments -> MY code
+- java doc comments -> GIVEN code
+********************************/
+
 import bridges.connect.Bridges;
 import bridges.base.Array1D;
 import bridges.base.Color;
 import bridges.base.Element;
 
 public class Setup {
+	
 	public static void main(String[] args) throws Exception {
 
 	    /* Initialize a Bridges connection with your credentials */
@@ -19,19 +26,27 @@ public class Setup {
 	    
 	    Array1D<Integer> arr = new Array1D<Integer> (arraySize);
 	    
-	    //for loop initialize each array entry to store a square number
-	    for (int i = 0; i < arraySize; i++) {
-	    	
-	    	int ele = (int) Math.pow(i, 2);
-	    	
-	    	arr.getElement(i).setLabel(String.valueOf(ele)); //CHANGED
-	    	
-	    }
-	    
-	    
-	    //.getElement(#) -> Access an individual element in the array
-	    //.setColor(" ")   -> To change color
-	   
+		//for loop initialize each array entry to store a square number
+	   	for (int i = 0; i < arraySize; i++) {
+	    		//creating a variable to contain the square numbers
+	    		int ele = (int) Math.pow(i, 2); //cast Math.pow as an int becuase the method is a double and the elements in the array need to be ints
+		
+			//Assigning the arrays elements - helpful code is given below w/ java doc comments above them	
+			arr.getElement(i).setLabel(String.valueOf(ele)); //CHANGED
+			 /***************************************************************************************
+			 .getElement(i) - This will fill in the array at i index
+			 .setLabel(...) - This will add the number into the index box
+			 String.valueOf(ele) - Since setLabel() is a String method we need to make the value 
+			    			(given by valueOf(ele)) a String as well because it is now an int
+			 ****************************************************************************************/
+		}
+
+	   	
+	    /************************************************************
+	    Lab Bonus - Change the color of the boxes
+	    .getElement(#) -> Access an individual element in the array
+	    .setColor(" ")   -> To change color
+	    ************************************************************/
 	    arr.getElement(0).setColor("deeppink");
 	    arr.getElement(1).setColor("mediumseagreen");
 	    arr.getElement(2).setColor("beige");
@@ -41,13 +56,7 @@ public class Setup {
 	    arr.getElement(6).setColor("violet");
 	    arr.getElement(7).setColor("wheat");
 	    arr.getElement(8).setColor("lavenderblush");
-	    
-	    
-	    
-	    
-	    
-	    
-	    
+	       
 	    /* Populate the array with integers */
 	    /* TODO: Make the array store square numbers*/
 	    arr.getElement(0).setValue(0);
@@ -60,5 +69,6 @@ public class Setup {
 
 	    /* Visualize the Array */
 	    bridges.visualize();
+		
 	}
 }
